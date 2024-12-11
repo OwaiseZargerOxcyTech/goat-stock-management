ba1bb52764a70e57f348cdc315773e000d95767a
Owais Zarger <owaiszarger@Owais-Macbook-Air.local>
2024-12-11 18:12:40 +0530

    api basurl

--- src/api/config.js
+++ src/api/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:3000/api",
-  baseURL: "https://goat-stock-server.vercel.app/",
+  baseURL: "https://goat-stock-server.vercel.app/api",
 });
 
 export default api;
