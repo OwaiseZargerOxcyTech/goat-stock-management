2687686f11a6cb22ac60c32ccc10137932f62f9c
Newgitweb <newgit@oxcytech.com>
2024-12-12 19:53:46 +0530

    added backend vercel link in config

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:3000/api",
   // baseURL: "https://goat-stock-server.vercel.app/api",
-  baseURL: "https://goat-stock-server-ten.vercel.app/api",
+  baseURL: "https://goat-stock-server-beta.vercel.app/api",
 });
 
 export default api;
